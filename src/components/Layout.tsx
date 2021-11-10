75100f2e1c52033bfcc2264524a71a1eb7b68651
gojutin <gojutin@gmail.com>
2021-11-10 15:04:51 -0500

    feat: updated header to include product Id info

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,20 +1,23 @@
 export type LayoutProps = {
   spread: string;
+  productId: string;
   children?: React.ReactNode;
 };
 
 export const Layout = (props: LayoutProps) => {
-  const { children } = props;
   const headerFooterClassNames = "text1 surface1 p-4 border-b borderColor";
   return (
     <div className="flex flex-col min-h-screen">
       <header
         className={`flex justify-center items-center relative ${headerFooterClassNames}`}>
-        <p className="absolute left-4">Order Book</p>
+        <h1 id="orderbook" className="absolute left-4">
+          Order Book ({props.productId})
+        </h1>
+
         <p className="text2 hidden md:block">Spread {props.spread}</p>
       </header>
       <main className="flex-1">
-        <div className="container mx-auto p-4">{children || "My New Page"}</div>
+        <div className="container mx-auto p-4">{props.children}</div>
       </main>
     </div>
   );
