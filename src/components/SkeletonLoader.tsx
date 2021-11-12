c5c1f8b02fe412e4cf802d31a1bdfc0e5f3ab819
gojutin <gojutin@gmail.com>
2021-11-12 18:55:15 -0500

    fix: linting issue

--- src/components/SkeletonLoader.tsx
+++ src/components/SkeletonLoader.tsx
@@ -17,7 +17,8 @@ export const SkeletonLoader = (props: SkeletonLoaderProps) => {
       baseColor={isDarkMode ? "rgba(0,0,0,.5)" : "rgba(0,0,0,.1)"}
       highlightColor={
         props.variant === "ask" ? "rgba(255, 51, 0, .2)" : "rgb(0, 204, 0, .2)" // green or red
-      }>
+      }
+    >
       <p>
         <Skeleton count={props.rows} width="98%" />
       </p>
