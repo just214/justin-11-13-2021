8eddc637faa3925162410162a7833a7555b6680b
gojutin <gojutin@gmail.com>
2021-11-12 11:12:37 -0500

    feat: added dark mode detection (custom hook) used to determine skeleton loader colors

--- src/components/SkeletonLoader.tsx
+++ src/components/SkeletonLoader.tsx
@@ -1,4 +1,5 @@
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import { useDetectDarkMode } from "hooks/useDetectDarkMode";
 import "react-loading-skeleton/dist/skeleton.css";
 
 type SkeletonLoaderProps = {
@@ -7,11 +8,12 @@ type SkeletonLoaderProps = {
 };
 
 export const SkeletonLoader = (props: SkeletonLoaderProps) => {
+  const isDarkMode = useDetectDarkMode();
   return (
     <SkeletonTheme
-      baseColor="rgba(0,0,0,.5)"
+      baseColor={isDarkMode ? "rgba(0,0,0,.5)" : "rgba(0,0,0,.1)"}
       highlightColor={
-        props.variant === "ask" ? "rgba(255, 51, 0, .2)" : "rgb(0, 204, 0, .2)"
+        props.variant === "ask" ? "rgba(255, 51, 0, .2)" : "rgb(0, 204, 0, .2)" // green or red
       }>
       <p>
         <Skeleton count={props.rows} width="98%" />
---
+++ src/hooks/useDetectDarkMode.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+
+export function useDetectDarkMode() {
+  const [isDarkMode, setIsDarkMode] = React.useState(false);
+
+  function handleUpdateState(e: { matches: boolean }) {
+    setIsDarkMode(e.matches);
+  }
+  React.useEffect(() => {
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addEventListener("change", handleUpdateState);
+
+    // Clean up
+    return () =>
+      window
+        .matchMedia("(prefers-color-scheme: dark)")
+        .removeEventListener("change", handleUpdateState);
+  }, []);
+
+  // Simply returns a boolean indicating if dark mode (all we need for this)
+  return isDarkMode;
+}
