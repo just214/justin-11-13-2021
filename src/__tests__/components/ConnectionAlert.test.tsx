d2fb45610a83487eb68843d8626049f23d1a866c
gojutin <gojutin@gmail.com>
2021-11-12 11:11:53 -0500

    test: added custom hooks tests and updates snapshots

--- src/__tests__/components/ConnectionAlert.test.tsx
+++ src/__tests__/components/ConnectionAlert.test.tsx
@@ -11,7 +11,11 @@ describe("ConnectionAlert", () => {
   <div
     class="bg-red-200 text-red-700 p-2 rounded-md my-4 flex flex-wrap items-center justify-center gap-4"
   >
-    Your were disconnected to reduce data usage.
+    <p
+      class="text-sm md:text-base"
+    >
+      Your were disconnected to reduce data usage.
+    </p>
     <button
       class="px-8 py-2 text-white rounded-md transition-colors disabled:opacity-50 disabled:cursor-not-allowed bg-gray-700 hover:bg-gray-800 undefined"
     >
